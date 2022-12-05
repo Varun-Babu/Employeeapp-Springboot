ea8d7624411b44c511425ac17d2914d1a2ab3376
Varun-Babu <itsme.varun.26@gmail.com>
2022-12-05 20:13:43 +0530

    initial commit

---
+++ src/main/java/com/example/employeeapp2/Employeeapp2Application.java
@@ -0,0 +1,14 @@
+package com.example.employeeapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeeapp2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Employeeapp2Application.class, args);
+	}
+
+}
